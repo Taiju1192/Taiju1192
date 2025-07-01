31fcc67eba54428c8b2cc5777c43af69e86a230f
Taiju1192 <netaka1192@gmail.com>
2025-07-01 20:06:43 +0900

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,9 @@
+import { SlashCommandBuilder } from 'discord.js';
+
 export default {
-  data: {
-    name: 'ping',
-    description: 'Replies with Pong!'
-  },
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
   async execute(interaction) {
     await interaction.reply('Pong!');
   }
