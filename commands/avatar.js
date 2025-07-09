8a097d97365bbc1eb92810407d2d290bb759891b
Taiju1192 <netaka1192@gmail.com>
2025-07-09 19:33:44 +0900

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('avatar')
-    .setDescription('プロフィール画像を表示する')
+    .setDescription('プロフィール画像を表示')
     .addUserOption(opt =>
       opt.setName('ユーザー')
         .setDescription('表示したいユーザー')
@@ -12,9 +12,11 @@ module.exports = {
   async execute(interaction) {
     const user = interaction.options.getUser('ユーザー') || interaction.user;
 
+    const avatarURL = user.displayAvatarURL({ dynamic: true, size: 512 });
+
     const embed = new EmbedBuilder()
       .setTitle('🖼️ アバター表示')
-      .setImage(user.displayAvatarURL({ size: 512, extension: 'png' }))
+      .setImage(avatarURL)
       .setColor(0x7289da)
       .setFooter({ text: `ユーザー → ${user.tag}` });
 
