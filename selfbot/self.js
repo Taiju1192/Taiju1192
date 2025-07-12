7b061b16dd4afba0a28c2fd07dcee2e8587c0c12
Taiju1192 <netaka1192@gmail.com>
2025-07-12 21:05:08 +0900

    Update self.js

--- selfbot/self.js
+++ selfbot/self.js
@@ -8,19 +8,19 @@ selfClient.on('ready', () => {
   console.log(`🟢 セルフボット起動: ${selfClient.user.tag}`);
 
   selfClient.user.setPresence({
-    activities: [
-      {
-        name: 'nakano mikuを推し中',
-        type: 'CUSTOM',
-        state: '中野三玖',
-        assets: {
-          largeImage: 'nakano_miku_img', // Developer Portal で登録したキー名
-          largeText: '中野三玖'
-        }
+  activities: [
+    {
+      name: 'nakano mikuを推し中',
+      type: 'CUSTOM',
+      state: '中野三玖',
+      assets: {
+        largeImage: 'nakano_miku_img', // ✅ Discord Developer Portal に登録したキー名
+        largeText: '中野三玖'
       }
-    ],
-    status: 'online'
-  });
+    }
+  ],
+  status: 'online'
+});
 });
 
 selfClient.login(process.env.SELF_TOKEN);
