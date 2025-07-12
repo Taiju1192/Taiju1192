371cbb440b4ed822c0f65efda41f92896bedd99d
Taiju1192 <netaka1192@gmail.com>
2025-07-12 22:53:49 +0900

    Update self.js

--- selfbot/self.js
+++ selfbot/self.js
@@ -3,9 +3,21 @@ require('dotenv').config();
 
 const selfClient = new Client();
 
-selfClient.on('ready', () => {
+selfClient.on('ready', async () => {
   console.log(`🟢 セルフボット起動: ${selfClient.user.tag}`);
-  // ステータスやプレゼンスの設定は不要
+
+  selfClient.user.setPresence({
+    status: 'online',
+    activities: [
+      {
+        name: '中野三玖を推し中',
+        type: 0, // 0 = Playing, 2 = Listening, 3 = Watching
+        details: '今日も推し活',
+      }
+    ],
+  });
+
+  console.log('🎮 アクティビティを設定しました');
 });
 
 selfClient.login(process.env.SELF_TOKEN);
